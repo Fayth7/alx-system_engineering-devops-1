ad354485df380ba355657aac889a7609e7fc2058
Fayth7 <flexflakes@gmail.com>
2023-07-04 05:03:29 -0700

    updates

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,21 +1,3 @@
 #!/usr/bin/env ruby
-
-# Read the log file path from command-line argument
-log_file = ARGV[0]
-
-# Open the log file
-File.open(log_file, 'r') do |file|
-  # Read each line in the log file
-  file.each_line do |line|
-    # Extract the relevant information using regex
-    match_data = line.match(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/)
-    if match_data
-      sender = match_data[1]
-      receiver = match_data[2]
-      flags = match_data[3]
-
-      # Print the extracted information
-      puts "#{sender},#{receiver},#{flags}"
-    end
-  end
-end
+#Auth: Sangwani P Zyambo
+puts ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/).join(",")
